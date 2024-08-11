8d705bffd54bf791e925198eef085433579a4e1b
Tristan Ross <tristan.ross@midstall.com>
2024-08-10 20:55:08 -0700

    feat(pkgs): save more of the user environment

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -21,28 +21,69 @@ in
         mount -t 9p home "$HOME" -o trans=virtio,version=9p2000.L,msize=131072,mode=ro
       fi
 
+      if [[ ! -n "$dontBindProfiles" ]]; then
+        export PATH=${util-linux}/bin:${coreutils}/bin
+
+        mkdir -p /etc/profiles
+        mount -t 9p etc-profiles /etc/profiles -o trans=virtio,version=9p2000.L,msize=131072,mode=ro
+
+        mkdir -p /etc/static/profiles
+        mount -t 9p etc-static-profiles /etc/static/profiles -o trans=virtio,version=9p2000.L,msize=131072,mode=ro
+      fi
+
+      if [[ ! -n "$dontBindNixOS" ]]; then
+        export PATH=${util-linux}/bin:${coreutils}/bin
+
+        mkdir -p /run/current-system
+        mount -t 9p nixos-system /run/current-system -o trans=virtio,version=9p2000.L,msize=131072,mode=ro
+      fi
+
       source /tmp/xchg/saved-env
+      echo "$HOSTNAME" >/proc/sys/kernel/hostname
 
       export name=${name}
       export shellHook=${drv.shellHook or ""}
 
       export OLD_PWD=$PWD
+      export OLD_PATH=$PATH
       unset QEMU_OPTS
 
       cd $OLD_PWD
       unset OLD_PWD
 
       source ${stdenv}/setup
+      export PATH=$PATH:$OLD_PATH
+      unset OLD_PATH
+
       exec ${busybox}/bin/setsid ${bashInteractive}/bin/bash < /dev/${qemu-common.qemuSerialDevice} &> /dev/${qemu-common.qemuSerialDevice}
     '';
 
     shellHook = ''
+      if [[ ! -e /etc/profiles ]]; then
+        export dontBindProfiles=1
+      fi
+
+      if [[ ! -e /run/current-system ]]; then
+        export dontBindNixOS=1
+      fi
+
+      export HOSTNAME=$(hostname)
+
       mkdir -p $TMPDIR/xchg
       export > $TMPDIR/xchg/saved-env
-      echo "declare -x \"PS1=$PS1\"" >> $TMPDIR/xchg/saved-env
+      echo "declare -x PS1=\"$PS1\"" >> $TMPDIR/xchg/saved-env
 
       if [[ ! -n "$dontBindHome" ]]; then
         export QEMU_OPTS="$QEMU_OPTS -virtfs local,path=$HOME,security_model=none,mount_tag=home,readonly=on"
+      fi
+
+      if [[ ! -n "$dontBindProfiles" ]]; then
+        export QEMU_OPTS="$QEMU_OPTS -virtfs local,path=/etc/profiles,security_model=none,mount_tag=etc-profiles,readonly=on"
+        export QEMU_OPTS="$QEMU_OPTS -virtfs local,path=/etc/static/profiles,security_model=none,mount_tag=etc-static-profiles,readonly=on"
+      fi
+
+      if [[ ! -n "$dontBindNixOS" ]]; then
+        export QEMU_OPTS="$QEMU_OPTS -virtfs local,path=/run/current-system,security_model=none,mount_tag=nixos-system,readonly=on"
       fi
 
       ${pkgs.vmTools.qemuCommandLinux}
