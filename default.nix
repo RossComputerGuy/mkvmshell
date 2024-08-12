66fb7763f90c4c5a77ba4c112ffbb70d4aa7dae7
Tristan Ross <tristan.ross@midstall.com>
2024-08-11 21:48:38 -0700

    feat(default): add vm shell

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
 let
   flakeLock = builtins.fromJSON (builtins.readFile ./flake.lock);
   nixpkgs = builtins.fetchTree flakeLock.nodes.nixpkgs.locked;
-in (import nixpkgs.outPath)
+in
+{ overlays ? [], config ? {}, ... }@args:
+(import nixpkgs.outPath) ({
+  overlays = [
+    (import ./pkgs/default.nix)
+  ] ++ args.overlays or [];
+  config = {
+    useVMShell = true;
+  } // args.config or {};
+} // builtins.removeAttrs args [ "overlays" "config" ])
